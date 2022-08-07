fce8b503534a767b8ed5e2a5eebceda4a270b8f3
uzaygokay <uzaygokay@gmail.com>
2022-08-07 03:33:30 +0200

    changed the folders

---
+++ src/models/gan-bert/train_gan-bert.py
@@ -0,0 +1,38 @@
+#%%imports
+
+#pip install transformers==4.3.2
+import torch
+import io
+import torch.nn.functional as F
+import random
+import numpy as np
+import time
+import math
+import datetime
+import torch.nn as nn
+from transformers import *
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+#!pip install torch==1.7.1+cu101 torchvision==0.8.2+cu101 -f https://download.pytorch.org/whl/torch_stable.html
+#!pip install sentencepiece
+
+##Set random values
+seed_val = 42
+random.seed(seed_val)
+np.random.seed(seed_val)
+torch.manual_seed(seed_val)
+if torch.cuda.is_available():
+  torch.cuda.manual_seed_all(seed_val)
+
+#%%check gpu
+# If there's a GPU available...
+if torch.cuda.is_available():    
+    # Tell PyTorch to use the GPU.    
+    device = torch.device("cuda")
+    print('There are %d GPU(s) available.' % torch.cuda.device_count())
+    print('We will use the GPU:', torch.cuda.get_device_name(0))
+# If not...
+else:
+    print('No GPU available, using the CPU instead.')
+    device = torch.device("cpu")
+
+#%%
